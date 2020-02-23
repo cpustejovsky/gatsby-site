83ddf8ff62b4a98444081977ed47c51504929f60
cpustejovsky <charles.pustejovsky@gmail.com>
2020-02-22 20:23:07 -0500

    initial commit for gatsby site; installed Bootstrap; familiarizing myself with Gatsby;

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,35 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
+import Nav from "react-bootstrap/Nav"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const Header = () => (
+  <Nav className="justify-content-center" activeKey="/home">
+    <Nav.Item>
+      <Nav.Link href="/">Home</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link href="/page-2">Page 2</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link
+        href="https://cpustejovsky.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Current Site
+      </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link
+        href="https://dev.to/cpustejovsky"
+        target="_blank"
+        rel="noreferrer"
+      >
+        My Blog
+      </Nav.Link>
+    </Nav.Item>
+  </Nav>
 )
 
 Header.propTypes = {
@@ -36,7 +37,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `Test`,
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -24,23 +25,17 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+    <div className="site-container">
+      <Header
+        siteTitle="Cpustejovsky" /*siteTitle={data.site.siteMetadata.title}*/
+      />
+      <div>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
-    </>
+      <footer className="footer">
+        Cpustejovsky, {new Date().getFullYear()}
+      </footer>
+    </div>
   )
 }
 
