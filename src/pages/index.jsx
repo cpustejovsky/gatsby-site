d7196dadc4a4f3b20f99feae54c4171f62326a8e
cpustejovsky <charles.pustejovsky@gmail.com>
2020-02-26 19:20:06 -0500

    adding projects component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,21 @@ import React from "react"
 import Layout from "../components/layout.jsx"
 import SEO from "../components/seo"
 import Introduction from "../components/subpages/introduction"
+import ReadingList from "../components/subpages/reading"
+import About from "../components/subpages/about"
+import Projects from "../components/subpages/projects"
 //TODO: make use of gatsby-image https://www.gatsbyjs.org/packages/gatsby-image/
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Introduction />
+    <br/>
+    <About />
+    <br/>
+    <Projects />
+    <br/>
+    <ReadingList />
+    <br/>
   </Layout>
 )
 
