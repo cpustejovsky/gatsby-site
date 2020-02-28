390a75013d52d4018302452ab089c1bddce18db4
cpustejovsky <charles.pustejovsky@gmail.com>
2020-02-27 19:06:01 -0500

    updating nav; adding font awesome cdn

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,14 +6,21 @@ import React from "react"
 const Header = () => (
   <>
     <style></style>
-    <Navbar bg="header" variant="light" expand="lg" sticky="top">
+    <Navbar bg="header" variant="light" expand="lg" sticky="top" className="mr-auto">
       <Navbar.Brand href="/">Cpustejovsky</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
-          <Nav.Link href="#link">About Me</Nav.Link>
-          <Nav.Link href="#link">Projects</Nav.Link>
-          <Nav.Link href="#link">Resources</Nav.Link>
+          <Nav.Link href="#about">About Me</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#reading">Reading List</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav>
+          <Nav.Link href="#">Icon</Nav.Link>
+          <Nav.Link href="#">Icon</Nav.Link>
+          <Nav.Link href="#">Icon</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
