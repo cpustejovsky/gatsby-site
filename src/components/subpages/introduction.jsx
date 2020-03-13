8000763fa1e3a42ab0327ae4231569134aa9f7ab
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-13 13:03:14 -0400

    edited introduction

--- src/components/subpages/introduction.jsx
+++ src/components/subpages/introduction.jsx
@@ -11,26 +11,22 @@ const Introduction = () => (
       </Col>
       <Col lg={6}>
         <h3>Howdy!</h3>
-        <p>
-          I'm Charles Pustejovsky. I'm working to align my career path with my
+        <p>  
+          I'm looking for a front-end or full-stack developer role to align my career path with my
           <strong> ikigai</strong>. Ikigai is something you're passionate about,
           talented at, can have a career in, and can help the world with.
         </p>
         <p>
-          After years of searching and learning,
+          After years of searching,
+          {" "}
           <strong>
             <Link to="https://medium.com/@cpustejovsky/do-what-you-love-445711971a3">
               I realized my ikigai is programming.
             </Link>
+          {" "}
           </strong>
-          Since then I've been learning as much as I can about computer science,
-          JavaScript, and how to use code to create things that help myself and
-          others.
-        </p>
-        <p>
-          I'm a self-taught software engineer looking for a{" "}
-          <strong>full-stack</strong> or <strong>back-end</strong> developer
-          role to put my passion and skills to work.
+          Since then I've learned as much as I can about computer science,
+          JavaScript, and how to use code to create things that help others.
         </p>
         <hr />
         <div>
