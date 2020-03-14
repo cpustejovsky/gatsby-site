9fc8bf3d4136131d649f4033e6bc845d7fd4b2dc
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-14 12:15:55 -0400

    adding link for ikigai

--- src/components/subpages/introduction.jsx
+++ src/components/subpages/introduction.jsx
@@ -13,13 +13,23 @@ const Introduction = () => (
         <p>
           I'm looking for a front-end or full-stack developer role to align my
           career path with my
-          <strong> ikigai</strong>. Ikigai is something you're passionate about,
-          talented at, can have a career in, and can help the world with.
+          <a
+            rel="noreferrer noopener"
+            href="https://www.forbes.com/sites/chrismyers/2018/02/23/how-to-find-your-ikigai-and-transform-your-outlook-on-life-and-business/#4bb1f0872ed4"
+          >
+            {" "}
+            ikigai
+          </a>
+          . Ikigai is something you're passionate about, talented at, can have a
+          career in, and can help the world with.
         </p>
         <p>
           After years of searching,{" "}
           <strong>
-            <a rel="noreferrer noopener" href="https://medium.com/@cpustejovsky/do-what-you-love-445711971a3">
+            <a
+              rel="noreferrer noopener"
+              href="https://medium.com/@cpustejovsky/do-what-you-love-445711971a3"
+            >
               I realized my ikigai is programming.
             </a>{" "}
           </strong>
