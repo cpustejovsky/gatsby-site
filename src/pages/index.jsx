4ed3d7d388656ab142c500caf4c17c3da15b1416
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-14 11:26:47 -0400

    clean up comment and format json

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,6 @@ import Introduction from "../components/subpages/introduction"
 import ReadingList from "../components/subpages/reading"
 import About from "../components/subpages/about"
 import Projects from "../components/subpages/projects"
-//TODO: make use of gatsby-image https://www.gatsbyjs.org/packages/gatsby-image/
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
