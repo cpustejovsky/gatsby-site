18e2ad3b63ef3f62eec19171ba8fbd68cd3b09a4
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-17 07:44:10 -0400

    updating introduction

--- src/components/subpages/introduction.jsx
+++ src/components/subpages/introduction.jsx
@@ -4,39 +4,38 @@ import Ikigai from "../images/ikigai"
 import Resume from "../docs/Cpustejovsky_Resume.pdf"
 const Introduction = () => (
   <Container>
-    <Row>
+    <Row className="introduction__items">
       <Col lg={6}>
         <Ikigai />
       </Col>
       <Col lg={6}>
-        <h3>Howdy!</h3>
+        <p>Howdy! I'm Charles Pustejovsky and <strong>programming is my ikigai</strong></p>
+        <hr />
         <p>
-          I'm looking for a front-end or full-stack developer role to align my
-          career path with my{" "}
+          An{" "}
           <a
             rel="noreferrer noopener"
             target="_blank"
             href="https://www.forbes.com/sites/chrismyers/2018/02/23/how-to-find-your-ikigai-and-transform-your-outlook-on-life-and-business/#4bb1f0872ed4"
           >
             ikigai
-          </a>
-          . Ikigai is something you're passionate about, talented at, can have a
-          career in, and can help the world with.
+          </a>{" "}
+          is something you're passionate about, talented at, can have a career
+          in, and can help the world with.
         </p>
         <p>
-          After years of searching,{" "}
-          <strong>
-            <a
-              rel="noreferrer noopener"
-              target="_blank"
-              href="https://dev.to/cpustejovsky/a-month-of-development-work-52gh"
-            >
-              I realized that programming is my ikigai.
-            </a>{" "}
-          </strong>
-          Since then I've learned as much as I can about computer science,
-          JavaScript, and how to use code to create things that help others
-          while building <a href="#projects">projects</a> along the way.
+          I've loved learning as much as I can about computer science and
+          JavaScript. I'm thankful that I've been able to{" "}
+          <a
+            rel="noreferrer noopener"
+            target="_blank"
+            href="https://dev.to/cpustejovsky/a-month-of-development-work-52gh"
+          >
+            provide value to my former company with programming
+          </a>
+          . I'm hopeful that I'll be able to find a job where I provide value
+          with programming full-time. Until then, I'll keep building{" "}
+          <a href="#projects">projects</a> along the way.
         </p>
         <hr />
         <div>
