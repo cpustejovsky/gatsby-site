cd5a72a6a66c2d77af033aa0b729df274dbc0752
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-20 12:20:36 -0400

    using gtag instad of ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,17 @@ module.exports = {
         trackingId: "UA-147269757-1",
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-XXXXXXXX-X`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
