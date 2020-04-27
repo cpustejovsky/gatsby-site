59edae220767f26b7d0c64d30e8f9ffaad2790ff
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-27 13:47:35 -0400

    copy edits to landing page

--- src/components/subpages/help.jsx
+++ src/components/subpages/help.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Row, Col, Button } from "react-bootstrap"
+import { Button } from "react-bootstrap"
 import FNV_EXP from "../images/fnv_exp"
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 
@@ -50,93 +50,68 @@ const CustomForm = ({ status, message, onValidated }) => {
 }
 
 const HelpWanted = () => (
-  <Container>
-    <Row>
-      <Col md={12}>
-        <h2>Help Me Level Up my Development</h2>
-        <FNV_EXP />
-        <br></br>
-        <p>
-          Since I started looking for a development job, the main feedback I’ve
-          received from the places I’ve applied to is that I don’t have the
-          amount of experience they are looking for.
-        </p>
-        <p>
-          Maybe it’s all the time I’ve spent playing RPGs, but whenever I see
-          that reply, I think “Okay, now how do I make my experience go up? How
-          do I level up as a developer?”
-        </p>
-        <p>
-          <strong>
-            {" "}
-            I’m only going to level up by doing the work. By building projects.
-            By contributing to larger projects. By learning more about the
-            technologies I’m using and their underlying principles.
-          </strong>
-        </p>
-        <p>
-          I can build an app. I can make it work. However, because I care about
-          writing quality code, questions arise:
-        </p>
-        <ul>
-          <li>Did I choose a good solution?</li>
-          <li>Is the code elegant?</li>
-          <li>Is it readable?</li>
-          <li>Is it maintainable?</li>
-          <li>Is the user experience solid?</li>
-        </ul>
-        <p>
-          <strong>This is where you can help, if you’re willing.</strong>
-        </p>
-        <p>
-          If you have experience with CSS, React, NodeJS, or MongoDB, you can
-          check out my{" "}
-          <a
-            href="https://github.com/cpustejovsky/estuary"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            source code
-          </a>{" "}
-          and offer any recommendations or critiques.
-        </p>
-        <p>
-          If you have experience with design, you can take a tour of my app and
-          see what could make for better UX.
-        </p>
-        <p>
-          If you’re just an everyday person with no technical skills, great!
-          That’s who I’m likely to be building apps for professionally so your
-          thoughts are the most invaluable of all!
-        </p>
-        <p>
-          The app in question is{" "}
-          <a
-            href="https://cpustejovsky-estuary.herokuapp.com/"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            Estuary
-          </a>
-          , a note taking app that I’m rebuilding into a productivity app
-          that’ll help me be a more effective developer and a more effective
-          person.
-        </p>
-        <hr />
-        <h3>Sign up to follow Estuary's progress</h3>
-        <MailchimpSubscribe
-          url={url}
-          render={({ subscribe, status, message }) => (
-            <CustomForm
-              status={status}
-              message={message}
-              onValidated={formData => subscribe(formData)}
-            />
-          )}
+  <>
+    <h2>Help Me Level Up!</h2>
+    <FNV_EXP />
+    <br></br>
+    <p>
+      I often think about how I would like to have more development experience.
+      With all the time I spent playing RPGs, my reaction to that thought is
+      “Okay, how can I increase my experience? How do I level up as a
+      developer?”
+    </p>
+    <p>
+      <strong>
+        I can level up by doing the work, by building projects, and by
+        contributing to open-source projects.
+      </strong>
+    </p>
+    <p>
+      I’m already building Estuary, a productivity app that’ll help me be a more
+      effective developer and a more effective person. As I build Estuary, these
+      questions arise:
+    </p>
+    <ul>
+      <li>Did I choose a good solution for ___?</li>
+      <li>Is the code readable? Is it maintainable?</li>
+      <li>Is the user experience solid?</li>
+    </ul>
+    <p>
+      <strong>This is where you can help.</strong>
+    </p>
+    <p>
+      I’ll be writing a bi-weekly newsletter with updates and new features. If
+      you’re willing to help me grow this app and level up as a developer, sign
+      up to follow Estuary's progress.
+    </p>
+    <p>
+      If you have developer experience, you can check out{" "}
+      <a
+        href="https://github.com/cpustejovsky/estuary"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        source code
+      </a>{" "}
+      and offer recommendations.
+    </p>
+    <p>
+      If you have experience with design, you can take a tour of my app and see
+      what could make for better UX.
+    </p>
+    <hr />
+    <h3>Sign up to Receive Updates on Estuary</h3>
+    <MailchimpSubscribe
+      url={url}
+      render={({ subscribe, status, message }) => (
+        <CustomForm
+          status={status}
+          message={message}
+          onValidated={formData => subscribe(formData)}
         />
-      </Col>
-    </Row>
-  </Container>
+      )}
+    />
+  </>
 )
 
 export default HelpWanted
