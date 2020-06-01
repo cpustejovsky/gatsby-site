a6107cdb5a6f574997ed2f9abb387177695ceac9
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-01 15:57:42 -0400

    testing useEffect in layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, {useState, useEffect} from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -15,6 +15,16 @@ import "../scss/layout.scss"
 import "../scss/custom-bootstrap.scss"
 
 const Layout = ({ children }) => {
+
+  const [starsCount, setStarsCount] = useState(0)
+useEffect(() => {
+  // get data from GitHub api
+  fetch(`https://api.github.com/repos/gatsbyjs/gatsby`)
+    .then(response => response.json()) // parse JSON from request
+    .then(resultData => {
+      setStarsCount(resultData.stargazers_count)
+    }) // set data for the number of stars
+}, [])
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -33,7 +43,7 @@ const Layout = ({ children }) => {
           {children}{" "}
           <footer className="site__footer">
             Cpustejovsky <i class="far fa-thumbs-up"></i>,{" "}
-            {new Date().getFullYear()}
+            {new Date().getFullYear()}; Fetch Test: {starsCount}
           </footer>
         </main>
       </div>
