02345065143d3f2077580beb2b63fe2cb8af0a39
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-01 13:09:06 -0400

    moving back to class based component

--- src/pages/resources.jsx
+++ src/pages/resources.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { Component } from "react"
 import { Container } from "react-bootstrap"
 import Header from "../components/header.jsx"
 import SEO from "../components/seo"
@@ -8,54 +8,58 @@ import parse from "html-react-parser"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../scss/layout.scss"
 import "../scss/custom-bootstrap.scss"
-const Resources = () => {
-  const [apiData, setApiData] = useState("")
-  useEffect(() => {
-    fetch(`https://dev.to/api/articles/281175`)
-      .then(res => res.json())
-      .then(data => {
-        setApiData(data.body_html)
-      })
-  }, [])
-  return (
-    <>
-      <SEO title="Resources | Cpustejovsky" />
-      <Header />
-      <div className="site">
-        <main className="site__landing">
-          <Container>
-            <h1>Resources</h1>
-            <ARCHIVIST />
-            <br></br>
-            {parse(apiData)}
-            <hr />
-            <p>
-              Resources are hosted on{" "}
-              <a
-                rel="noreferrer noopener"
-                target="_blank"
-                href="https://dev.to/"
-              >
-                DEV.to
-              </a>{" "}
-              and fetched using the{" "}
-              <a
-                rel="noreferrer noopener"
-                target="_blank"
-                href="https://docs.dev.to/api/"
-              >
-                DEV Community API
-              </a>
-              .
-            </p>
-          </Container>
-          <footer className="site__footer">
-            Cpustejovsky <i className="far fa-thumbs-up"></i>,{" "}
-            {new Date().getFullYear()}
-          </footer>
-        </main>
-      </div>
-    </>
-  )
+export default class Resources extends Component {
+  constructor() {
+    super()
+    this.state = { data: "" }
+  }
+
+  async componentDidMount() {
+    let post = await axios.get(`https://dev.to/api/articles/281175`)
+    let data = post.data.body_html
+    this.setState({ data: data })
+  }
+
+  render() {
+    return (
+      <>
+        <SEO title="Resources | Cpustejovsky" />
+        <Header />
+        <div className="site">
+          <main className="site__landing">
+            <Container>
+              <h1>Resources</h1>
+              <ARCHIVIST />
+              <br></br>
+              {parse(this.state.data)}
+              <hr />
+              <p>
+                Resources are hosted on{" "}
+                <a
+                  rel="noreferrer noopener"
+                  target="_blank"
+                  href="https://dev.to/"
+                >
+                  DEV.to
+                </a>{" "}
+                and fetched using the{" "}
+                <a
+                  rel="noreferrer noopener"
+                  target="_blank"
+                  href="https://docs.dev.to/api/"
+                >
+                  DEV Community API
+                </a>
+                .
+              </p>
+            </Container>
+            <footer className="site__footer">
+              Cpustejovsky <i className="far fa-thumbs-up"></i>,{" "}
+              {new Date().getFullYear()}
+            </footer>
+          </main>
+        </div>
+      </>
+    )
+  }
 }
-export default Resources
