5408eba1a1fd5e6d3e0fd0a8183884aadf7d45b2
cpustejovsky <charles.pustejovsky@gmail.com>
2020-06-01 10:47:55 -0400

    refactoring resources into functional component

--- src/pages/resources.jsx
+++ src/pages/resources.jsx
@@ -1,41 +1,38 @@
-import React, { Component } from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import React, { useState, useEffect } from "react"
+import { Container } from "react-bootstrap"
 import Header from "../components/header.jsx"
 import SEO from "../components/seo"
 import ARCHIVIST from "../components/images/archivist"
 import axios from "axios"
 import parse from "html-react-parser"
-export default class resources extends Component {
-  constructor() {
-    super()
-    this.state = { data: "" }
-  }
+const Resources = () => {
+  const [data, setData] = useState("")
 
-  async componentDidMount() {
+  useEffect(async () => {
     let post = await axios.get(`https://dev.to/api/articles/281175`)
     let data = post.data.body_html
-    this.setState({ data: data })
-  }
-  render() {
-    return (
-      <>
-        <SEO title="Charles Pustejovsky | Full-Stack Developer" />
-        <Header />
-        <div className="site">
-          <main className="site__landing">
-            <Container>
-              <h1>Resources</h1>
-              <ARCHIVIST/>
-              <br />
-              {parse(this.state.data)}
-            </Container>
-            <footer className="site__footer">
-              Cpustejovsky <i className="far fa-thumbs-up"></i>,{" "}
-              {new Date().getFullYear()}
-            </footer>
-          </main>
-        </div>
-      </>
-    )
-  }
+    setData(data)
+  }, [])
+
+  return (
+    <>
+      <SEO title="Resources | Cpustejovsky" />
+      <Header />
+      <div className="site">
+        <main className="site__landing">
+          <Container>
+            <h1>Resources</h1>
+            <ARCHIVIST />
+            <br></br>
+            {parse(data)}
+          </Container>
+          <footer className="site__footer">
+            Cpustejovsky <i className="far fa-thumbs-up"></i>,{" "}
+            {new Date().getFullYear()}
+          </footer>
+        </main>
+      </div>
+    </>
+  )
 }
+export default Resources
