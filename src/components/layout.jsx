2e6b42ff09127eb9a53f6299e393f28bcbb50cb5
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-26 19:41:18 -0400

    updating Header to ts (nothing to change really)

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header.jsx"
+import Header from "./Header.tsx"
 import "../scss/layout.scss"
 
 const Layout = ({ children }) => {
