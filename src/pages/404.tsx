34e02233240423a5a85b7d7cc3ff1533cce13ae5
cpustejovsky <charles.pustejovsky@gmail.com>
2020-10-14 13:03:10 -0400

    removing overseer font to improve performance

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Container } from "@material-ui/core"
+import { Container, Typography } from "@material-ui/core"
 import Fallout404 from "../components/images/fallout404"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <Container className="site site__404">
       <div>
-        <h1 className="fallout">Page Not Found</h1>
+        <Typography variant="h1">Page Not Found</Typography>
         <Fallout404 />
       </div>
     </Container>
